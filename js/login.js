5d26841aaa769a692cfa92e1a2c659e3edd6f76c
Lourdes Billone <lou.billone@gmail.com>
2024-01-27 16:24:21 -0300

    creacion de pages registro

---
+++ js/login.js
@@ -0,0 +1,14 @@
+// let correo = document.getElementById("text-email");
+// let pass = document.getElementById("text-password");
+
+// const handleSubmit = (e) => {
+//   e.preventDefault();
+
+//   inicioSesion()
+// };
+
+// const inicioSesion = ()=>{
+
+// }
+
+// document.getElementById("formulario").addEventListener("submit", handleSubmit);
