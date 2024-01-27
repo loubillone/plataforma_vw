847e4ed7ad067d986f12d131d479b0605f689014
Lourdes Billone <lou.billone@gmail.com>
2024-01-27 16:56:44 -0300

    realizo lógica de registro en js

---
+++ js/registro.js
@@ -0,0 +1,52 @@
+let usuarios = JSON.parse(localStorage.getItem("usuarios")) || [];
+
+class Usuario {
+  constructor(nombre, email, contraseña, rol = "usuario") {
+    this.nombre = nombre;
+    this.email = email;
+    this.contraseña = contraseña;
+    this.rol = rol;
+  }
+}
+
+//FUNCION QUE DETIENE QUE SE REFRESQUE LA PÁGINA Y ADEMÁS LLAMA A LA FUNCIÓN DE REGISTRO
+const handleSubmit = (e) => {
+  e.preventDefault();
+
+  registroUsuario();
+};
+
+//FUNCION DE REGISTRO
+
+const registroUsuario = () => {
+  let nombre = document.getElementById("text-nombre").value;
+  let correo = document.getElementById("text-email").value;
+  let password = document.getElementById("text-password").value;
+  let password2 = document.getElementById("text-password2").value;
+
+  if (password != password2) {
+    return alert("No coinciden las contraseñas");
+  }
+
+  let validar = usuarios.find((user) => {
+    return user.email === correo;
+  });
+
+  if (validar) {
+    return alert(
+      "El correo ya está registrado, inicie sesión con sus credenciales"
+    );
+  }
+
+  usuarios.push(new Usuario(nombre, correo, password));
+
+  localStorage.setItem("usuarios", JSON.stringify(usuarios));
+
+  document.getElementById("formulario").reset();
+
+  alert("Usuario registrado con éxito");
+
+  location.replace("../index.html");
+};
+
+document.getElementById("formulario").addEventListener("submit", handleSubmit);
